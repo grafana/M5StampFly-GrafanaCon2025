4dcb20cad8ea1cfa46688046b2fe965fdc17048b
wengelbrecht-grafana <willie.engelbrecht@grafana.com>
2025-02-21 15:34:36 +0000

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,58 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Kouhei Ito
+ * Copyright (c) 2024 M5Stack
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include <WiFi.h>
+#include <Arduino.h>
+#include <FastLED.h>
+#include "flight_control.hpp"
+
+// VL53L0X_ADDRESS           0x29
+// MPU6886_ADDRESS           0x68
+// BMP280_ADDRESS            0x76
+
+// Wi-Fi credentials
+const char* ssid = ""; // Use a 2.4Ghz Wi-Fi hotspot
+const char* password = "";
+
+void setup() {
+    init_copter();
+    delay(100);
+
+    // Connect to Wi-Fi (Station Mode)
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to Wi-Fi");
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(1000);
+    }
+    Serial.println("\nWi-Fi connected!");
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void loop() {
+    loop_400Hz();
+}
